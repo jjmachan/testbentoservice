8abe5642125648c934706533c1552b9e315e6c8d
jjmachan <jamesjithin97@gmail.com>
2021-12-09 12:25:43 +0530

    refactored bentoservice folders

--- bentoservice/build.py
+++
@@ -1,5 +0,0 @@
-import bentoml
-
-from service import SERVICE_NAME
-
-bentoml.build('./service.py:svc')
---
+++ build.py
@@ -0,0 +1,6 @@
+import bentoml
+
+from service import SERVICE_NAME
+
+bentoml.bentos.build_bentofile('./bentofile.yaml')
+bentoml.export_bento(SERVICE_NAME, '../saved_dir')
