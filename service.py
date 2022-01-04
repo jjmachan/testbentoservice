4a72d6e472a0cd49533228d8be223e072ac33821
jjmachan <jamesjithin97@gmail.com>
2022-01-04 19:50:46 +0530

    added other IO descriptors

--- service.py
+++ service.py
@@ -1,5 +1,5 @@
 import bentoml
-from bentoml.io import JSON
+from bentoml.io import JSON, NumpyNdarray, PandasDataFrame, File, Multipart, Text
 
 SERVICE_NAME = "testservice"
 svc = bentoml.Service(SERVICE_NAME)
@@ -7,8 +7,29 @@ svc = bentoml.Service(SERVICE_NAME)
 
 @svc.api(input=JSON(), output=JSON())
 async def json(input_json):
+    print(input_json)
     return {"input_received": input_json, "foo": "bar"}
 
 
-# make sure to expose the asgi_app from the service instance
-# app = svc.asgi_app
+@svc.api(input=NumpyNdarray(), output=NumpyNdarray())
+async def ndarray(input_ndarray):
+    print(input_ndarray)
+    return input_ndarray
+
+
+@svc.api(input=PandasDataFrame(), output=PandasDataFrame())
+async def pandas(input_pandas):
+    print(input_pandas)
+    return input_pandas
+
+
+@svc.api(input=File(), output=File())
+async def file(input_file):
+    print(input_file)
+    return input_file
+
+
+@svc.api(input=Multipart(text=Text()), output=Text())
+async def multipart(text):
+    print(text)
+    return "ok"
